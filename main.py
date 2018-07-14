97ec1f2896490e3748a5bf4d83def5bdd8adc15e
akasan <akapikumin100piki@gmail.com>
2018-07-14 14:53:48 +0900

    capture

---
+++ main.py
@@ -0,0 +1,17 @@
+import cv2
+import numpy as np
+import pyrealsense2 as rs
+
+cap = cv2.VideoCapture(1)
+
+while(True):
+    ret, frame = cap.read()
+
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+    cv2.imshow('frame', frame)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+cap.release()
+cv2.destroyAllWindows()
