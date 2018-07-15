6199cff2acfc194f4a51be41d17f174bd5628116
akasan <akapikumin100piki@gmail.com>
2018-07-15 15:38:31 +0900

    範囲予測

--- main.py
+++ main.py
@@ -8,6 +8,9 @@ H = 480
 #画像の中心の座標
 CENTER = [int(H / 2), int(W / 2)]
 
+M = [0.15, 0.30]
+k = 1.0e5
+
 # Configure depth and color streams
 pipeline = rs.pipeline()
 config = rs.config()
@@ -37,20 +40,30 @@ try:
         color_image = np.asanyarray(color_frame.get_data())
 
         #中心座標の距離を取得
-        distance = depth_image[CENTER[0]][CENTER[1]] * depth_scale
+        distance = depth * depth_scale
         print(str(distance) + 'm')
 
+        #画像中心にペットボトルがあった場合のペットボトルの範囲を予測
+        depth = depth_image[CENTER[0]][CENTER[1]]
+        if distance != 0.0 && distance <= 2:
+            width = int(k * M[0] / depth) + 20
+            height = int(k * M[1] / depth) + 10
+            depth_center = depth_image[CENTER[0]-(height/2):CENTER[0]+(height/2), CENTER[1]-(width/2):CENTER[1]+(width/2)]
+            
+        else:
+            continue
+
         # Apply colormap on depth image (image must be converted to 8-bit per pixel first)
-        depth_colormap = cv2.applyColorMap(cv2.convertScaleAbs(depth_image, alpha=0.03), cv2.COLORMAP_JET)
+        #depth_colormap = cv2.applyColorMap(cv2.convertScaleAbs(depth_image, alpha=0.03), cv2.COLORMAP_JET)
 
         # Stack both images horizontally
-        images = np.hstack((color_image, depth_colormap))
+        #images = np.hstack((color_image, depth_colormap))
 
         # Show images
-        cv2.namedWindow('RealSense', cv2.WINDOW_AUTOSIZE)
-        cv2.imshow('RealSense', images)
-        if cv2.waitKey(1) & 0xFF == ord('q'):
-            break
+        #cv2.namedWindow('RealSense', cv2.WINDOW_AUTOSIZE)
+        #cv2.imshow('RealSense', images)
+        #if cv2.waitKey(1) & 0xFF == ord('q'):
+        #    break
 
 finally:
     # Stop streaming
