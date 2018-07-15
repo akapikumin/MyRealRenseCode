29c2dc725200684b61227ae63ed6409815fdf9f5
akasan <akapikumin100piki@gmail.com>
2018-07-15 19:06:42 +0900

    関数にまとめる

--- main.py
+++ main.py
@@ -1,71 +1,56 @@
-import cv2
 import numpy as np
 import pyrealsense2 as rs
 
-#幅と高さ
-W = 640
-H = 480
-#画像の中心の座標
-CENTER = [int(H / 2), int(W / 2)]
+def Start_stream():
+    #幅と高さ
+    W = 640
+    H = 480
+    #画像の中心の座標
+    CENTER = [int(H / 2), int(W / 2)]
 
-M = [0.15, 0.30]
-k = 1.0e5
+    # Configure depth and color streams
+    pipeline = rs.pipeline()
+    config = rs.config()
+    config.enable_stream(rs.stream.depth, W, H, rs.format.z16, 30)
 
-# Configure depth and color streams
-pipeline = rs.pipeline()
-config = rs.config()
-config.enable_stream(rs.stream.depth, W, H, rs.format.z16, 30)
-config.enable_stream(rs.stream.color, W, H, rs.format.bgr8, 30)
+    # Start streaming
+    profile = pipeline.start(config)
 
-# Start streaming
-profile = pipeline.start(config)
+    #Depthスケール取得
+    #距離[m] = depth * depth_scale
+    depth_sensor = profile.get_device().first_depth_sensor()
+    depth_scale = depth_sensor.get_depth_scale()
 
-#Depthスケール取得
-#距離[m] = depth * depth_scale
-depth_sensor = profile.get_device().first_depth_sensor()
-depth_scale = depth_sensor.get_depth_scale()
+    return pipeline, depth_scale, CENTER
 
-try:
+def get_distancce(pipeline, depth_scale, CENTER):
     while True:
-
-        # Wait for a coherent pair of frames: depth and color
         frames = pipeline.wait_for_frames()
         depth_frame = frames.get_depth_frame()
-        color_frame = frames.get_color_frame()
-        if not depth_frame or not color_frame:
-            continue
+        if depth_frame:
+            break
 
-        # Convert images to numpy arrays
-        depth_image = np.asanyarray(depth_frame.get_data())
-        color_image = np.asanyarray(color_frame.get_data())
+    # Convert images to numpy arrays
+    depth_image = np.asanyarray(depth_frame.get_data())
 
-        #中心座標の距離を取得
-        distance = depth * depth_scale
-        print(str(distance) + 'm')
+    #中心座標の距離を取得
+    depth = depth_image[CENTER[0]][CENTER[1]]
+    cedis = depth * depth_scale
 
-        #画像中心にペットボトルがあった場合のペットボトルの範囲を予測
-        depth = depth_image[CENTER[0]][CENTER[1]]
-        if distance != 0.0 && distance <= 2:
-            width = int(k * M[0] / depth) + 20
-            height = int(k * M[1] / depth) + 10
-            depth_center = depth_image[CENTER[0]-(height/2):CENTER[0]+(height/2), CENTER[1]-(width/2):CENTER[1]+(width/2)]
-            
-        else:
-            continue
+    height = 150
+    width = 70
+    depth_center_image = depth_image[CENTER[0]-height/2:CENTER[0]+height/2, CENTER[1]-width/2:CENTER[1]+width/2]
+    depth_center_image = depth_center_image - depth
+    avedis = np.average(depth_center_image) * depth_scale
 
-        # Apply colormap on depth image (image must be converted to 8-bit per pixel first)
-        #depth_colormap = cv2.applyColorMap(cv2.convertScaleAbs(depth_image, alpha=0.03), cv2.COLORMAP_JET)
+    return cedis, avedis
 
-        # Stack both images horizontally
-        #images = np.hstack((color_image, depth_colormap))
+pp, ds, cent = Start_stream()
+while True:
+    cedis, avedis = get_distancce(pp, ds, cent)
+    print(str(cedis) + 'm\n' + str(avedis) + 'm')
+    #i = input("続行:1　中断:1以外  ")
+    #if i != '1':
+        #break
 
-        # Show images
-        #cv2.namedWindow('RealSense', cv2.WINDOW_AUTOSIZE)
-        #cv2.imshow('RealSense', images)
-        #if cv2.waitKey(1) & 0xFF == ord('q'):
-        #    break
-
-finally:
-    # Stop streaming
-    pipeline.stop()
-    cv2.destroyAllWindows()
+pp.stop()
