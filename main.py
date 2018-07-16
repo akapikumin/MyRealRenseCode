73e12e703c521ca348ff47529de06e35faef2cef
akasan <akapikumin100piki@gmail.com>
2018-07-16 23:08:02 +0900

    平均をとろうとしている

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import numpy as np
 import pyrealsense2 as rs
+import cv2
 
 def Start_stream():
     #幅と高さ
@@ -12,6 +13,7 @@ def Start_stream():
     pipeline = rs.pipeline()
     config = rs.config()
     config.enable_stream(rs.stream.depth, W, H, rs.format.z16, 30)
+    config.enable_stream(rs.stream.color, W, H, rs.format.bgr8, 30)
 
     # Start streaming
     profile = pipeline.start(config)
@@ -23,34 +25,45 @@ def Start_stream():
 
     return pipeline, depth_scale, CENTER
 
-def get_distancce(pipeline, depth_scale, CENTER):
-    while True:
-        frames = pipeline.wait_for_frames()
-        depth_frame = frames.get_depth_frame()
-        if depth_frame:
-            break
-
-    # Convert images to numpy arrays
-    depth_image = np.asanyarray(depth_frame.get_data())
-
+def get_distancce(depth_image, depth_scale, CENTER):
     #中心座標の距離を取得
     depth = depth_image[CENTER[0]][CENTER[1]]
     cedis = depth * depth_scale
 
     height = 150
     width = 70
-    depth_center_image = depth_image[CENTER[0]-height/2:CENTER[0]+height/2, CENTER[1]-width/2:CENTER[1]+width/2]
+    depth_center_image = depth_image[int(CENTER[0]-height/2):int(CENTER[0]+height/2), int(CENTER[1]-width/2):int(CENTER[1]+width/2)]
     depth_center_image = depth_center_image - depth
+    #print(depth_center_image, depth)
     avedis = np.average(depth_center_image) * depth_scale
+    ran = [height, width]
 
-    return cedis, avedis
+    return cedis, avedis, ran
 
 pp, ds, cent = Start_stream()
 while True:
-    cedis, avedis = get_distancce(pp, ds, cent)
+    frames = pp.wait_for_frames()
+    depth_frame = frames.get_depth_frame()
+    color_frame = frames.get_color_frame()
+    if not depth_frame or not color_frame:
+        continue
+    depth_image = np.asanyarray(depth_frame.get_data())
+    color_image = np.asanyarray(color_frame.get_data())
+
+    cedis, avedis, rang = get_distancce(depth_image, ds, cent)
     print(str(cedis) + 'm\n' + str(avedis) + 'm')
-    #i = input("続行:1　中断:1以外  ")
-    #if i != '1':
-        #break
+
+    # Apply colormap on depth image (image must be converted to 8-bit per pixel first)
+    depth_colormap = cv2.applyColorMap(cv2.convertScaleAbs(depth_image, alpha=0.03), cv2.COLORMAP_JET)
+
+    # Stack both images horizontally
+    images = np.hstack((color_image, depth_colormap))
+
+    # Show images
+    cv2.namedWindow('RealSense', cv2.WINDOW_AUTOSIZE)
+    cv2.imshow('RealSense', images)
+
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
 
 pp.stop()
