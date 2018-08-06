0b310483e415c9d4b5ed869ff5bf769773473651
akasan <akapikumin100piki@gmail.com>
2018-08-06 15:53:35 +0900

    距離と対象の大きさによって範囲を予測

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import numpy as np
 import pyrealsense2 as rs
 import cv2
+import math
 
 def Start_stream():
     #幅と高さ
@@ -8,6 +9,8 @@ def Start_stream():
     H = 480
     #画像の中心の座標
     CENTER = [int(H / 2), int(W / 2)]
+    #カメラ上の大きさ算出 S = k(M/r)の比例定数kは画角と画像サイズで決まる
+    k = np.array([W / (2 * math.tan(math.radians(34.7))), H / (2 * math.tan(math.radians(21.25)))], dtype=np.float32)
 
     # Configure depth and color streams
     pipeline = rs.pipeline()
@@ -23,27 +26,27 @@ def Start_stream():
     depth_sensor = profile.get_device().first_depth_sensor()
     depth_scale = depth_sensor.get_depth_scale()
 
-    return pipeline, depth_scale, CENTER
+    return pipeline, depth_scale, CENTER, k
 
-def get_distancce(depth_image, depth_scale, CENTER):
+def get_distancce(depth_image, depth_scale, CENTER, k, V):
     #中心座標の距離を取得
     depth = depth_image[CENTER[0]][CENTER[1]]
     cedis = depth * depth_scale
+    if cedis == 0:
+        return cedis, 'none', [0, 0]
 
-    height = 150
-    width = 70
-    depth_center_image = np.array(depth_image[int(CENTER[0]-height/2):int(CENTER[0]+height/2), int(CENTER[1]-width/2):int(CENTER[1]+width/2)], dtype=np.int32)
+    ran = k * V / cedis
+    depth_center_image = np.array(depth_image[int(CENTER[0]-ran[1]/2):int(CENTER[0]+ran[1]/2), int(CENTER[1]-ran[0]/2):int(CENTER[1]+ran[0]/2)], dtype=np.int32)
     #depth_cent_colormap = cv2.applyColorMap(cv2.convertScaleAbs(depth_center_image, alpha=0.03), cv2.COLORMAP_JET)
     #depth_cent_colormap = cv2.resize(depth_cent_colormap, (140, 300))
     #cv2.imshow('cent', depth_cent_colormap)
     #print(depth_center_image, depth, type(depth))
     depth_center_image = depth_center_image - depth
     avedis = depth_center_image.mean() * depth_scale
-    ran = [height, width]
 
     return cedis, avedis, ran
 
-pp, ds, cent = Start_stream()
+pp, ds, cent, k = Start_stream()
 while True:
     frames = pp.wait_for_frames()
     depth_frame = frames.get_depth_frame()
@@ -53,12 +56,12 @@ while True:
     depth_image = np.asanyarray(depth_frame.get_data())
     color_image = np.asanyarray(color_frame.get_data())
 
-    cedis, avedis, rang = get_distancce(depth_image, ds, cent)
+    cedis, avedis, rang = get_distancce(depth_image, ds, cent, k, np.array([500e-3, 300e-3], dtype=np.float32))
     print(str(cedis) + 'm\n' + str(avedis) + 'm')
 
     # Apply colormap on depth image (image must be converted to 8-bit per pixel first)
     depth_colormap = cv2.applyColorMap(cv2.convertScaleAbs(depth_image, alpha=0.03), cv2.COLORMAP_JET)
-    depth_colormap = cv2.rectangle(depth_colormap, (int(cent[1]-rang[1]/2), int(cent[0]-rang[0]/2)), (int(cent[1]+rang[1]/2), int(cent[0]+rang[0]/2)), (0, 0, 0))
+    depth_colormap = cv2.rectangle(depth_colormap, (int(cent[1]-rang[0]/2), int(cent[0]-rang[1]/2)), (int(cent[1]+rang[0]/2), int(cent[0]+rang[1]/2)), (0, 0, 0))
     # Stack both images horizontally
     images = np.hstack((color_image, depth_colormap))
 
