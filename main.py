6cc07b7b7e06a3be45fa799b39573239f20698e0
akasan <akapikumin100piki@gmail.com>
2018-08-10 14:24:50 +0900

    範囲予測を横方向に限定

--- main.py
+++ main.py
@@ -10,8 +10,8 @@ def Start_stream():
     #画像の中心の座標
     CENTER = [int(H / 2), int(W / 2)]
     #カメラ上の大きさ算出 S = k(M/r)の比例定数kは画角と画像サイズで決まる
-    c = [8.0, 0.5]
-    k = np.array([W / (2 * math.tan(math.radians(34.7-c[0]))), H / (2 * math.tan(math.radians(21.25-c[1])))], dtype=np.float32)
+    c = 8.0
+    k = W / (2 * math.tan(math.radians(34.7-c)))
 
     # Configure depth and color streams
     pipeline = rs.pipeline()
@@ -29,7 +29,7 @@ def Start_stream():
 
     return pipeline, depth_scale, CENTER, k
 
-def get_distancce(depth_image, depth_scale, CENTER, k, V):
+def get_distancce(depth_image, depth_scale, CENTER, k, Vw):
     #中心座標の距離を取得
     depth = depth_image[CENTER[0]][CENTER[1]]
     cedis = depth * depth_scale
@@ -40,8 +40,8 @@ def get_distancce(depth_image, depth_scale, CENTER, k, V):
     cediff = float(depth) - float(depth_image[CENTER[0]][CENTER[1]+1])
 
     #深度の横方向に対する傾きによって、V[0]の値を少し変更する
-    V[0] = V[0] * math.cos(math.atan(abs(cediff)))
-    ran = k * V / cedis
+    Vw = Vw * math.cos(math.atan(abs(cediff)))
+    ran = [k * Vw / cedis, 20]
     # if cediff != 0:
     #     #高さの最小値に範囲の高さを合わせる→一番遠い場所で高さの計算をする
     #     ran = np.array([0, 0], dtype = np.float32)
@@ -76,7 +76,7 @@ while True:
     depth_image = np.asanyarray(depth_frame.get_data())
     color_image = np.asanyarray(color_frame.get_data())
 
-    cedis, avedif, rang = get_distancce(depth_image, ds, cent, k, np.array([500e-3, 300e-3], dtype=np.float32))
+    cedis, avedif, rang = get_distancce(depth_image, ds, cent, k, 500e-3)
     print(str(cedis) + 'm\n' + str(avedif))
 
     # Apply colormap on depth image (image must be converted to 8-bit per pixel first)
@@ -84,7 +84,7 @@ while True:
     depth_colormap = cv2.rectangle(depth_colormap, (int(cent[1]-rang[0]/2), int(cent[0]-rang[1]/2)), (int(cent[1]+rang[0]/2), int(cent[0]+rang[1]/2)), (0, 0, 0))
     # Stack both images horizontally
     images = np.hstack((color_image, depth_colormap))
-    #images = cv2.flip(images, 1)
+    images = cv2.flip(images, 1)
     # Show images
     cv2.namedWindow('RealSense', cv2.WINDOW_AUTOSIZE)
     cv2.imshow('RealSense', images)
